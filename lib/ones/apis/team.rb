e427c102dbf998c5ab7b2a5030e42a06ba93c3f3
David Ding <cod7ce@gmail.com>
2022-12-12 17:40:02 +0800

    CHG: change batch teams api HTTP Method

--- lib/ones/apis/team.rb
+++ lib/ones/apis/team.rb
@@ -8,8 +8,9 @@ module Ones
 
       # 批量获取团队信息
       # 当指定的team_uuid不存在时，返回的map中不会包含该团队的信息，即返回的map大小不一定等于传入的uuid数量
+      # 单次 100 个 uuid
       def batch(org_uuid, team_uuids)
-        get "app_platform/open_api/org/#{org_uuid}/team/batch", params: { team_uuids: team_uuids.join(',') }
+        post "app_platform/open_api/org/#{org_uuid}/teams/batch", params: { team_uuids: team_uuids }
       end
 
       # 获取应用管理员列表
--- lib/ones/apis/user.rb
+++ lib/ones/apis/user.rb
@@ -2,16 +2,19 @@ module Ones
   module Apis
     module User
       # 批量获取指定用户信息
+      # 单次 100 个 uuid
       def batch(org_uuid, user_uuids)
         post "app_platform/open_api/org/#{org_uuid}/user/batch", { user_uuids: user_uuids }
       end
 
       # 批量获取指定用户的所属团队
+      # 单次 50 个 uuid
       def teams(org_uuid, user_uuids)
         post "app_platform/open_api/org/#{org_uuid}/team_member/batch", { user_uuids: user_uuids }
       end
 
       # 批量获取用户在指定团队的所属部门
+      # 单次 50 个 uuid
       def departments(team_uuid, user_uuids)
         post "app_platform/open_api/team/#{team_uuid}/department_member/batch", { user_uuids: user_uuids }
       end
