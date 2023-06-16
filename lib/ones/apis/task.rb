0434c33685c947a42f02720956cd5c8272386137
David Ding <cod7ce@gmail.com>
2023-06-16 15:15:15 +0800

    ADD: task list query

---
+++ lib/ones/apis/task.rb
@@ -0,0 +1,26 @@
+module Ones
+  module Apis
+    module Task
+      def list(team_uuid, uuids: [])
+        query = %(
+          {
+            tasks(filter: $filter) {
+              uuid
+              name
+              statusCategory
+              issueType {
+                detailType
+                icon
+                name
+              }
+            }
+          }
+        )
+        post "project/team/#{team_uuid}/items/graphql", {
+          query: query,
+          variables: { filter: { uuid_in: uuids } }
+        }
+      end
+    end
+  end
+end
